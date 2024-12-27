bba590d9e19263fbe8e7f5e7daa544379cd5c3a6
nyenz-jona <nyenzjona@gmail.com>
2024-12-27 03:51:34 +0300

    NO 3
    GROSS PROFIT

--- USING FUNCTIONS.c
+++ USING FUNCTIONS.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-//NO 2
+//NO 3
 
-void calculate_sale_price(float original_price, float discount_percentage, float *sale_price) {
-    *sale_price = original_price - (original_price * discount_percentage / 100);
-}
-void calculate_salary_after_tax(float original_salary, float tax_rate, float *final_salary) {
-    *final_salary = original_salary - (original_salary * tax_rate / 100);
+
+void calculategrossprofit(double salesrevenue, double cogs, double grossprofit) {
+    grossprofit = salesrevenue - cogs;
 }
 
 int main() {
-    float original_salary = 5000000.0;
-    float tax_rate = 15.0;
-    float final_salary;
+    double salesrevenue = 15000000;
+    double cogs = 9000000;
+    double grossprofit;
 
-    calculate_salary_after_tax(original_salary, tax_rate, &final_salary);
+    calculategrossprofit(salesrevenue, cogs, grossprofit);
 
-    printf("Salary After Tax: UGX %.2f\n", final_salary);
+    printf("Gross Profit: UGX %.2lf", grossprofit);
 
     return 0;
 }
