93ef397ac8423817a27060725576913f66409e3d
nyenz-jona <nyenzjona@gmail.com>
2024-12-27 03:35:18 +0300

    NO2 2
    TOTAL SALE PRICE

--- USING FUNCTIONS.c
+++ USING FUNCTIONS.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-//NO 1
+//NO 2
 
-
-void totalsaleprice(double originalprice, double discount, double saleprice) {
-    saleprice = originalprice - (originalprice * discount);
+void calculate_sale_price(float original_price, float discount_percentage, float *sale_price) {
+    *sale_price = original_price - (original_price * discount_percentage / 100);
+}
+void calculate_salary_after_tax(float original_salary, float tax_rate, float *final_salary) {
+    *final_salary = original_salary - (original_salary * tax_rate / 100);
 }
 
 int main() {
-    double originalprice = 200000.0;
-    double discount = 0.1;
-   double saleprice;
+    float original_salary = 5000000.0;
+    float tax_rate = 15.0;
+    float final_salary;
 
-   totalsaleprice( originalprice, discount, saleprice);
+    calculate_salary_after_tax(original_salary, tax_rate, &final_salary);
 
-    printf("Sale Price: UGX %.2f\n", saleprice);
+    printf("Salary After Tax: UGX %.2f\n", final_salary);
 
     return 0;
 }
